8e99d036686363733148d51ad504e6a2afbf5251
tjdgus3160 <tjdgus3160@gmail.com>
2021-01-24 11:31:25 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -7,7 +7,7 @@ class Customer extends React.Component{
         return (
             <TableRow>
                 <TableCell>{this.props.id}</TableCell>
-                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell><img src={this.props.image} alt="profile" height='60' width='60'/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
